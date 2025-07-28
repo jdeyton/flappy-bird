a59ee6c6acbb5e39df13610b19d1207e8f818b41
jdeyton <jordan.deyton@gmail.com>
2025-07-27 23:29:24 -0400

    Adds initial Player class implementation.

---
+++ public/Player.js
@@ -0,0 +1,18 @@
+
+class Player {
+  constructor() {
+    this.x = 50; // Default starting X position
+    this.y = 240; // Default starting Y position (center of 480 height)
+    this.velocityY = 0;
+  }
+
+  flap() {
+    // To be implemented later
+  }
+
+  update() {
+    // To be implemented later
+  }
+}
+
+module.exports = Player;
