7b70424d9dcb8be4798d89c2e52f94baaec95b71
jdeyton <jordan.deyton@gmail.com>
2025-07-27 23:29:24 -0400

    Adds Pipe constructor implementation.

---
+++ public/Pipe.js
@@ -0,0 +1,16 @@
+class Pipe {
+  constructor(canvasHeight, canvasWidth) {
+    this.width = 80;
+    this.gap = 200;
+    this.x = canvasWidth;
+    this.speedX = -3;
+
+    const yMin = 50;
+    const yMax = canvasHeight - this.gap - yMin;
+
+    // `y` is the position of the bottom edge of the upper pipe.
+    this.y = Math.floor(Math.random() * (yMax - yMin) + yMin);
+  }
+}
+
+module.exports = Pipe;
