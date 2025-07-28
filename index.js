00daef336563792b17f6b5c234840b3a117b3360
jdeyton <jordan.deyton@gmail.com>
2025-07-27 23:29:24 -0400

    Adds index with blank screen in 16:9 resolution.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+}
+
+module.exports = app;
